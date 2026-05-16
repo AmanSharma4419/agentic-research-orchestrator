f7e8e813ff1bddb021910c18eb008662009c2f8e
AmanSharma4419 <sharmaaman4419@gmail.com>
2026-05-16 22:10:54 +0530

    feat:implementation of langraph for query research

---
+++ app/graphs/research_graph.py
@@ -0,0 +1,64 @@
+from typing import TypedDict
+from app.agents.builder_research_agent import build_research_agent
+from app.agents.builder_evaluate_agent import build_evaluator_agent
+from app.prompts.evaluation_prompt import EVALUATION_USER_PROMPT
+
+from langgraph.graph import(StateGraph,START,END)
+
+research_agent = build_research_agent()
+evaluate_agent = build_evaluator_agent()
+
+class ResearchState(TypedDict):
+    query:str
+    research_answer:str
+    evaluation:str
+    
+    
+    
+async def research_node(state: ResearchState):
+        response = await research_agent.ainvoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": state["query"]
+                }
+            ]
+        }
+    )
+        research_response = response["messages"][-1].content
+        return {
+                 "research_answer": research_response
+              }
+        
+async def evaluation_node(state:ResearchState):
+        user_prompt = EVALUATION_USER_PROMPT.format(
+        query=state.get("query"),
+        research_response=state.get("research_answer")
+    ) 
+                       
+        evaluation_response = await evaluate_agent.ainvoke(
+           {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": user_prompt
+                }
+            ]
+        } 
+        )
+        return {
+            "evaluation" :evaluation_response
+        }
+        
+        
+graph_builder = StateGraph(ResearchState)
+
+graph_builder.add_node("research_node",research_node)
+graph_builder.add_node("evaluation_node",evaluation_node)
+
+graph_builder.add_edge(START,"research_node")
+graph_builder.add_edge("research_node","evaluation_node")
+graph_builder.add_edge("evaluation_node",END)
+
+research_graph = graph_builder.compile()
